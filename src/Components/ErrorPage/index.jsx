d05c62903ace08177fc3a88a38cc27a8e6c5f449
alseny-diallo <alsenydiallo0409@gmail.com>
2023-03-18 22:23:19 +0000

    feat: updating error page

--- src/Components/ErrorPage/index.jsx
+++ src/Components/ErrorPage/index.jsx
@@ -1,5 +1,19 @@
+import batman from '../../assets/images/batman.png';
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <div className="quiz-bg">
+      <div className="container">
+        <h2 style={{ textAlign: 'center', marginTop: '50px' }}>
+          Oups, cette page n'existe pas!
+        </h2>
+        <img
+          style={{ display: 'block', margin: '40px auto' }}
+          src={batman}
+          alt="batman"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default ErrorPage;
