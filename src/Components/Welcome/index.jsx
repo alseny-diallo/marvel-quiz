8176fd7925434f37578e9959d95148ff75b02950
alseny-diallo <alsenydiallo0409@gmail.com>
2023-04-04 13:14:06 +0000

    feat: adding user session

--- src/Components/Welcome/index.jsx
+++ src/Components/Welcome/index.jsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Logout from '../Logout';
 import Quiz from '../Quiz';
+import { onAuthStateChanged } from 'firebase/auth';
+import auth from '../Firebase/Firebase';
+import { useNavigate } from 'react-router-dom';
 
 const Welcome = () => {
+  const [userSession, setUserSession] = useState(null);
+  const navigate = useNavigate();
+  useEffect(() => {
+    const listener = onAuthStateChanged(auth, (user) => {
+      user ? setUserSession(user) : navigate('/');
+    });
+    return listener();
+  }, []);
   return (
-    <div className="quiz-bg">
-      <div className="container">
-        <Logout />
-        <Quiz />
-      </div>
-    </div>
+    <>
+      {!userSession ? (
+        <>
+          <div className="loader"></div>
+          <p>Loading...</p>
+        </>
+      ) : (
+        <div className="quiz-bg">
+          <div className="container">
+            <Logout />
+            <Quiz />
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
