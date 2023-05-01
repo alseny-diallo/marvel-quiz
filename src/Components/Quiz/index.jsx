6cb0bd6ad527ff0ddabe9154541e0f1c40a55648
alseny-diallo <alsenydiallo0409@gmail.com>
2023-05-01 20:29:43 +0000

    update

--- src/Components/Quiz/index.jsx
+++ src/Components/Quiz/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Levels from '../Levels';
 import ProgressBar from '../ProgressBar';
 import { QuizMarvel } from '../QuizMarvel';
@@ -13,11 +13,14 @@ const Quiz = ({ user }) => {
   const [idQuestion, setIdQuestion] = useState(0);
   const [isDisabled, setIsDisabled] = useState(true);
   const [userAnswer, setUserAnswer] = useState(null);
+  const [score, setScore] = useState(0);
+  const storedDataRef = useRef(null);
 
   useEffect(() => {
     const loadQuestions = (level) => {
       const fetchArrayQuizz = QuizMarvel[0].quizz[level];
       if (fetchArrayQuizz.length >= maxQuestions) {
+        storedDataRef.current = fetchArrayQuizz;
         const newArray = fetchArrayQuizz.map(({ answer, ...rest }) => rest);
         setStoredQuestions(newArray);
       } else {
@@ -29,16 +32,41 @@ const Quiz = ({ user }) => {
   }, [quizLevel, maxQuestions, levels]);
 
   useEffect(() => {
+    toast.info(`Welcome`, {
+      position: 'top-right',
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: false,
+    });
+  }, []);
+
+  useEffect(() => {
     if (storedQuestions.length > 0) {
       setQuestion(storedQuestions[idQuestion].question);
       setOptions(storedQuestions[idQuestion].options);
+      setUserAnswer(null);
+      setIsDisabled(true);
     }
+
     return () => {};
   }, [storedQuestions, idQuestion]);
 
   const submitAnswer = (selectedAnswer) => {
     setUserAnswer(selectedAnswer);
     setIsDisabled(false);
+  };
+
+  const loadNextQuestion = () => {
+    if (idQuestion === maxQuestions - 1) {
+      return null;
+    }
+    const goodAnswer = storedDataRef.current[idQuestion].answer;
+    setIdQuestion(idQuestion + 1);
+    if (userAnswer === goodAnswer) {
+      setScore(score + 1);
+    }
   };
 
   return (
@@ -57,7 +85,11 @@ const Quiz = ({ user }) => {
           {option}
         </p>
       ))}
-      <button className="btnSubmit" disabled={isDisabled}>
+      <button
+        className='btnSubmit'
+        onClick={loadNextQuestion}
+        disabled={isDisabled}
+      >
         Suivant
       </button>
     </div>
